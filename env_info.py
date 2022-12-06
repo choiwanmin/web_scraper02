434b1bb0f6f1ccbe32ff3c61f95d3ae455f757f9
choiwanmin <choi.wm512@gmail.com>
2022-12-06 17:41:33 +0900

    env_info 삭제

--- env_info.py
+++
@@ -1,8 +0,0 @@
-# web_scraper/settings.py 설정값
-SECRET_KEY = 'django-insecure-@3e15j&q$scs=7zprd2_9&=&&ts2a1a)#im^j!7#7ix8ewlce$'
-DEBUG = True
-ALLOWED_HOSTS = []
-
-# telegram 정보 : web_scraper/scripts/scraper.py
-TLGM_BOT_API = "5926578763:AAHlObcu3gOuIx9eWx5NymCgtdb5KVxTEME"
-chat_id = "2021682908"
